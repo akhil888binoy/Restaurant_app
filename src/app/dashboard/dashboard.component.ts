7571c490b2d688ab966570614416c3c1a65e3240
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-20 11:32:35 +0530

    template of table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  allRestaurants: Restaurant[]=[];
+  displayedColumns: string[] = ['id', 'name', 'owner', 'mobile', 'email', 'location'];
   constructor() { }
 
   ngOnInit(): void {
