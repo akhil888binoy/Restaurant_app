8e238134839a41d810970a205234217889a41dc3
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-20 12:04:22 +0530

    table

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '../restaurant';
+import { RestaurantService } from '../restaurant.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  allRestaurants: Restaurant[]=[];
+   allRestaurants: Restaurant[]= [];
   displayedColumns: string[] = ['id', 'name', 'owner', 'mobile', 'email', 'location'];
-  constructor() { }
+  constructor(private restaurant: RestaurantService) { }
 
   ngOnInit(): void {
+    this.getAllRestaurants();
   }
-
+  getAllRestaurants(){
+    this.restaurant.getAll().subscribe((data)=>{
+      this.allRestaurants = data;
+      console.log(this.allRestaurants);
+    })
+  }
 }
