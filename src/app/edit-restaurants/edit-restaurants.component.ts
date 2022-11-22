18083cd4d797fb2154df30793e041be452d8295f
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2022-11-22 18:24:03 +0530

    Restaurant service and activated route injected

--- src/app/edit-restaurants/edit-restaurants.component.ts
+++ src/app/edit-restaurants/edit-restaurants.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from '../restaurant';
 import { RestaurantService } from '../restaurant.service';
+import { Router , ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-restaurants',
@@ -17,9 +18,10 @@ export class EditRestaurantsComponent implements OnInit {
       location:''
 
     }
-  constructor() { }
+  constructor(private rs:RestaurantService, route:ActivatedRoute) { }
 
   ngOnInit(): void {
+
   }
 
 }
